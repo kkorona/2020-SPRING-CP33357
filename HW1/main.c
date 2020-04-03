9d921d1319e511ea25da505bc4fc779bdeb44fca
kkorona <high1uck@pusan.ac.kr>
2020-04-03 01:43:52 -0700

    fixed typos & added comments for main.c

--- HW1/main.c
+++ HW1/main.c
@@ -1,13 +1,15 @@
 // main.c -- CP33357 assignment #1, Spring 2020
-/ S. U. Park, id #201624476, April 3rd, 2020
+// S. U. Park, id #201624476, April 3rd, 2020
 
+
+// essentials.h -- includes essential header files for this program - ncurses.h
 #include "essentials.h"
 
 int main() {
     
-    init();
-    run();
-    clearResources();
+    init(); // initialize screen prefernce with ncurses libs.
+    run(); // run the main routine for timers.
+    clearResources(); // release the allocated resources for this program.
 
     return 0;
 }
